cdde6ce834ca2c45e26794b03a8269a50dcf4ec8
Rohit Bera <rohitbera7997@gmail.com>
2025-01-08 23:42:25 +0530

    commit class to className

--- src/Components/WhatWeDo.jsx
+++ src/Components/WhatWeDo.jsx
@@ -1,116 +1,95 @@
 function WhatWeDo() {
   return (
     <>
-      <section class="who-we-are-section overflow-hidden our-servicess">
-        <div class="section-2">
-          <figure class="who-we-right-fig">
-            <img
-              src="./assets/images/section2-before-img.png"
-              alt=""
-              class="star"
-            />
-          </figure>
-          <div class="container">
-            <div class="row">
-              <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12">
-                <div class="section2-img-content" data-aos="fade-up-right">
-                  <figure class="mb-0 who-we-figure section2-fig">
-                    <img src="./assets/images/section2-left-img.png" alt="" />
-                  </figure>
-                </div>
-              </div>
-              <div class="col-xl-7 col-lg-6 col-md-12 col-sm-12">
-                <div
-                  class="about-us-content aos-init aos-animate mr-0"
-                  data-aos="fade-up"
-                >
-                  <h6 class="autorix-text">What we do</h6>
-                  <h2
-                    data-aos="fade-zoom-in"
-                    class="aos-init aos-animate section2-h2"
-                  >
-                    The Perfect Solution For all Protection
-                  </h2>
-                  <p class="aboutus-p">
-                    Qugiat nulla pariatur excepteur sint occaecat cupidatat no
-                    proident, sunt in culpa rui officia deserunt.{" "}
-                  </p>
-                  <div class="section2-row1">
-                    <div class="row">
-                      <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="section-2-content">
-                          <figure class="section2-figure">
-                            <img
-                              src="./assets/images/section2-list-img.png"
-                              alt=""
-                            />
-                          </figure>
-                          <div class="section2-box">
-                            <h6 class="section2-heading">Cyber Solutions</h6>
-                            <p class="section2-text">
-                              Ruis autem vel eum rui inea elit niau.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="section-2-content row2-content">
-                          <figure class="section2-figure">
-                            <img
-                              src="./assets/images/section2-list-img.png"
-                              alt=""
-                            />
-                          </figure>
-                          <div class="section2-box">
-                            <h6 class="section2-heading">Network Security</h6>
-                            <p class="section2-text">
-                              Guis autem vel eum rui inea elit niau.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
+<section className="who-we-are-section overflow-hidden our-servicess">
+  <div className="section-2">
+    <figure className="who-we-right-fig">
+      <img src="./assets/images/section2-before-img.png" alt className="star" />
+    </figure>
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-5 col-lg-6 col-md-12 col-sm-12">
+          <div className="section2-img-content" data-aos="fade-up-right">
+            <figure className="mb-0 who-we-figure section2-fig">
+              <img src="./assets/images/section2-left-img.png" alt />
+            </figure>
+          </div>
+        </div>
+        <div className="col-xl-7 col-lg-6 col-md-12 col-sm-12">
+          <div className="about-us-content aos-init aos-animate mr-0" data-aos="fade-up">
+            <h6 className="autorix-text">What we do</h6>
+            <h2 data-aos="fade-zoom-in" className="aos-init aos-animate section2-h2">
+              The Perfect Solution For all Protection
+            </h2>
+            <p className="aboutus-p">
+              Qugiat nulla pariatur excepteur sint occaecat cupidatat no
+              proident, sunt in culpa rui officia deserunt.{'{'}" "{'}'}
+            </p>
+            <div className="section2-row1">
+              <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="section-2-content">
+                    <figure className="section2-figure">
+                      <img src="./assets/images/section2-list-img.png" alt />
+                    </figure>
+                    <div className="section2-box">
+                      <h6 className="section2-heading">Cyber Solutions</h6>
+                      <p className="section2-text">
+                        Ruis autem vel eum rui inea elit niau.
+                      </p>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                      <div class="section-2-content">
-                        <figure class="section2-figure">
-                          <img
-                            src="./assets/images/section2-list-img.png"
-                            alt=""
-                          />
-                        </figure>
-                        <div class="section2-box">
-                          <h6 class="section2-heading">Web Security</h6>
-                          <p class="section2-text">
-                            Muis autem vel eum rui inea elit niau.
-                          </p>
-                        </div>
-                      </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="section-2-content row2-content">
+                    <figure className="section2-figure">
+                      <img src="./assets/images/section2-list-img.png" alt />
+                    </figure>
+                    <div className="section2-box">
+                      <h6 className="section2-heading">Network Security</h6>
+                      <p className="section2-text">
+                        Guis autem vel eum rui inea elit niau.
+                      </p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                      <div class="section-2-content row2-content">
-                        <figure class="section2-figure">
-                          <img
-                            src="./assets/images/section2-list-img.png"
-                            alt=""
-                          />
-                        </figure>
-                        <div class="section2-box">
-                          <h6 class="section2-heading">Locker Security</h6>
-                          <p class="section2-text">
-                            Euis autem vel eum rui inea elit niau.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-sm-6">
+                <div className="section-2-content">
+                  <figure className="section2-figure">
+                    <img src="./assets/images/section2-list-img.png" alt />
+                  </figure>
+                  <div className="section2-box">
+                    <h6 className="section2-heading">Web Security</h6>
+                    <p className="section2-text">
+                      Muis autem vel eum rui inea elit niau.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-6">
+                <div className="section-2-content row2-content">
+                  <figure className="section2-figure">
+                    <img src="./assets/images/section2-list-img.png" alt />
+                  </figure>
+                  <div className="section2-box">
+                    <h6 className="section2-heading">Locker Security</h6>
+                    <p className="section2-text">
+                      Euis autem vel eum rui inea elit niau.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+  </div>
+</section>
+
     </>
   );
 }
