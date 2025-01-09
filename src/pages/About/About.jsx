4291de454497bbc075b459c12e7aa3a2b444ecdb
Rohit Bera <rohitbera7997@gmail.com>
2025-01-09 11:53:54 +0530

    Update in about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../includes/Navbar";
 import Footer from "../../includes/Footer";
 import Banner from "./Banner";
 import AboutUsSection from "../../Components/AboutUsSection";
-import WhyChooseusSection from "../../Components/WhyChooseUsSection";
+import WhyChooseusSection from "../../Components/WhyChooseusSection";
 import CtaSection from "../../Components/CtaSection";
 import CarouselSection from "../../Components/CarouselSection";
 import FormSection from "../../Components/FormSection";
